cd18665b831de6fe2a62a5b0e982614c4cfec526
Michi <michivonah@gmail.com>
2023-04-28 19:55:44 +0200

    create scripts

---
+++ setup_debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+
+# Debian Setup
+# Michi von Ah - April 2023
+
+# Run as "root"
+
+# Install updates
+apt-get update && apt-get upgrade -y
+
+# Install basic software
+apt-get install htop mc wget curl unzip ufw -y
+
+# Install docker
+apt-get install docker.io docker-compose -y
---
+++ setup_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+
+# Ubuntu Setup
+# Michi von Ah - April 2023
+
+# Run as "root"
+
+# Install updates
+apt update && apt upgrade -y
